2b8a99645ea0c3b5cab5da833b236de85eeb425e
Roman Vasilev <s91630277@gmail.com>
2016-07-26 16:17:42 +0300

    Updated files

--- sigh.js
+++ sigh.js
@@ -6,8 +6,8 @@ module.exports = function(pipelines) {
 
     pipelines["build"] = [
         glob({basePath: "src"}, "**/*.ts"),
-        ts({module: "commonjs", target: "es5"}),
+        ts({module: "commonjs", target: "es5", inlineSources: true}),
         write("lib"),
-        ava({files: "test/*.js", source: "src/**/*.ts"})
+        ava({files: "test/*.js", source: "src/**/*.ts", verbose: true})
     ];
 };
